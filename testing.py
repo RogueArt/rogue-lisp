cc99382ee2241addaa84ded1f6115e4b863e5362
Rogue Art <rogueart.dev@outlook.com>
2023-05-07 01:19:46 -0700

    Update code with integration tests for calling self and other method

--- testing.py
+++ testing.py
@@ -61,8 +61,17 @@ def get_test_programs():
     set_fields = ['(class puppy', '(method bark ()', '(print "woof!")', ')', ')', '(class main', '(field foo_123 10)', '(field name "unknown")', '(field _awesome true)', '(field obj_ref_field_puppy null)', '(method main ()',
                   '(begin', '(set foo_123 (+ 20 50))', '(set name true)', '(set _awesome 0)', '(set obj_ref_field_puppy (new puppy))' '(print "The value is " foo_123 " and " name " and " _awesome " and value is " (% 3 2)))', ')', ')']
 
+    # Basic call test - be able to instantiate object and call a method with no parameters on it
+    basic_other_call_test = ['(class puppy', '(method bark ()', '(print "woof!")', ')', ')', '(class main', '(field puppy_ref null)',
+                       '(method main ()', '(begin', '(set puppy_ref (new puppy))', '(call puppy_ref bark)', ')', ')', ')']
 
-    return {'simple': simple, 'many_fields': many_fields, 'simple_begin_test': simple_begin_test, 'nested_begin_test': nested_begin_test, 'set_fields': set_fields}
+    # Basic self call test - be able to call a method from own class
+    basic_self_call_test = ['(class main', '(method bark () (print "its a me, mario!"))',
+                       '(field puppy_ref null)', '(method main ()', '(begin', '(call me bark)', ')', ')', ')']
+    
+    # Nested call test - be able to call a method from own class
+
+    return {'simple': simple, 'many_fields': many_fields, 'simple_begin_test': simple_begin_test, 'nested_begin_test': nested_begin_test, 'basic_other_call_test': basic_other_call_test, 'basic_self_call-test': basic_self_call_test, 'set_fields': set_fields}
 
 # Deliberately small and obscure name for each easy debugging
 # Will pretty print the array with the given indentation level
