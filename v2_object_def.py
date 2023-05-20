68a922f295f318f9f6c5dfa6679dc37e1c7110e9
Rogue Art <rogueart.dev@outlook.com>
2023-05-20 13:20:55 -0700

    Add type hint to checkers

--- v2_object_def.py
+++ v2_object_def.py
@@ -86,31 +86,31 @@ class ObjectDefinition:
         return
 
     # <========== MATCH STATEMENT ============>
-    def is_a_print_statement(self, statement):
+    def is_a_print_statement(self, statement: Annotated[list[str], 1]) -> bool:
         return statement[0] == InterpreterBase.PRINT_DEF
 
-    def is_a_set_statement(self, statement):
+    def is_a_set_statement(self, statement: Annotated[list[str], 1]) -> bool:
         return statement[0] == InterpreterBase.SET_DEF
 
-    def is_an_input_statement(self, statement):
+    def is_an_input_statement(self, statement: Annotated[list[str], 1]) -> bool:
         return statement[0] == InterpreterBase.INPUT_INT_DEF or statement[0] == InterpreterBase.INPUT_STRING_DEF
 
-    def is_a_call_statement(self, statement):
+    def is_a_call_statement(self, statement: Annotated[list[str], 1]) -> bool:
         return statement[0] == InterpreterBase.CALL_DEF
 
-    def is_a_while_statement(self, statement):
+    def is_a_while_statement(self, statement: Annotated[list[str], 1]) -> bool:
         return statement[0] == InterpreterBase.WHILE_DEF
 
-    def is_an_if_statement(self, statement):
+    def is_an_if_statement(self, statement: Annotated[list[str], 1]) -> bool:
         return statement[0] == InterpreterBase.IF_DEF
 
-    def is_a_return_statement(self, statement):
+    def is_a_return_statement(self, statement: Annotated[list[str], 1]) -> bool:
         return statement[0] == InterpreterBase.RETURN_DEF
 
-    def is_a_begin_statement(self, statement):
+    def is_a_begin_statement(self, statement: Annotated[list[str], 1]) -> bool:
         return statement[0] == InterpreterBase.BEGIN_DEF
 
-    def is_a_let_statement(self, statement):
+    def is_a_let_statement(self, statement: Annotated[list[str], 1]) -> bool:
         return statement[0] == InterpreterBase.LET_DEF
 
     #  <========== GETTERS AND SETTERS ===========>
