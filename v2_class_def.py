82016e59240f1c593ec093d4fdf2a79cc43f446d
Rogue Art <rogueart.dev@outlook.com>
2023-05-19 20:03:04 -0700

    Split code into multiple files

---
+++ v2_class_def.py
@@ -0,0 +1,18 @@
+from bparser import BParser
+from v2_object_def import *
+import copy
+
+class ClassDefinition:
+    # constructor for a ClassDefinition
+    def __init__(self, interpreter, interpreter_base: InterpreterBase, name, methods, fields):
+        self.interpreter = interpreter
+        self.interpreter_base = interpreter_base
+        self.name = name
+        self.methods = methods
+        self.fields = fields
+
+    # uses the definition of a class to create and return an instance of it
+    def instantiate_object(self):
+        obj = ObjectDefinition(self.interpreter, self.interpreter_base, 
+                               copy.deepcopy(self.methods), copy.deepcopy(self.fields))
+        return obj
