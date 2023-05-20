bdf5db3056da47036d708670daa5d084699e7ef2
Rogue Art <rogueart.dev@outlook.com>
2023-05-19 21:45:10 -0700

    Add test for checking method types

--- manual_testing_v2.py
+++ manual_testing_v2.py
@@ -10,9 +10,26 @@ from pprint import pprint
 
 def get_test_programs():
     # Legacy test - Verify that program with field types can be parsed (legacy)
-    field_types = [
+#     field_types = [
+#   '(class person',
+#   '(method nothing ()',
+#   '(return "")',
+#   ')',
+#   ')',
+#   '(class main',
+#   '(field int x 10)',
+#   '(field string y "xyz")',
+#   '(field person p null)',
+#   '(method main ()',
+#   '(print x " " y)',
+#   ')',
+#   ')'
+# ]
+
+    # Verify parsing of return and parameter types
+    method_types = [
   '(class person',
-  '(method nothing ()',
+  '(method void nothing ()',
   '(return "")',
   ')',
   ')',
@@ -20,17 +37,38 @@ def get_test_programs():
   '(field int x 10)',
   '(field string y "xyz")',
   '(field person p null)',
-  '(method main ()',
-  '(print x " " y)',
+  '(method void main ()',
+  '(call me print_stuff x y p)',
+  ')',
+  '(method void print_stuff ((int a) (string b) (person p))',
+  '(print a " " b " " p)', # Note - we aren't expected to print the person object, but do it for testing anyways
+  ')',
+  ')'
+]
+    
+    method_types_with_obj = [
+  '(class person',
+  '(method void nothing ()',
+  '(return "")',
+  ')',
+  ')',
+  '(class main',
+  '(field int x 10)',
+  '(field string y "xyz")',
+  '(field person p null)',
+  '(method void main ()',
+  '(call me print_stuff x y)',
+  ')',
+  '(method void print_stuff ((int a) (string b) (person p))',
+  '(print a " " b)',
   ')',
   ')'
 ]
 
 
     return {
-        "field_types": field_types,
+        "method_types": method_types,
     }
-
 # Deliberately small and obscure name for each easy debugging
 # Will pretty print the array with the given indentation level
 
